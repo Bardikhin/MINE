2681ca0a0ff8e679f95305144f7680aaee2ecd7a
Bardikhin <32037797+Bardikhin@users.noreply.github.com>
2017-10-04 23:06:20 +0300

    lesson5

---
+++ lesson5.php
@@ -0,0 +1,31 @@
+<?php
+	$jsonString = file_get_contents('C:\xampp\htdocs\mine\phoneBook.json');
+	$data = json_decode($jsonString, true);
+?>
+
+<html>
+<head>
+	<title>Телефонная книга</title>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<td>Имя</td>
+			<td>Фамилия</td>
+			<td>Адрес</td>
+			<td>Номер телефона</td>
+		</tr>
+		<?php foreach ($data as $datum) {?>
+			<tr>
+				<td><?php echo $datum['firstName']?></td>
+				<td><?php echo $datum['lastName']?></td>
+				<td><?php echo $datum['address']?></td>
+				<td><?php echo $datum['phoneNumber']?></td>
+			</tr>
+		<?php } ?>
+
+	</table>
+
+</body>
+</html>
