25aad29b6f52c5fc2d50168cd31dfa3be1dd991c
Bardikhin <32037797+Bardikhin@users.noreply.github.com>
2017-10-15 23:07:05 +0300

    NA RABOTU 15.10

--- admin.php
+++ admin.php
@@ -1,4 +1,20 @@
-<form method="post" action="test.php" enctype="multipart/form-fata">
-	файл <input type="file" name="myfile"><br/>
+<form method="post" action="" enctype="multipart/form-data">
+	<input type="file" name="uploadFile"><br/>
+	<input  type="hidden" name="MAX_FILE_SIZE" value="900000000"  /><br/>
 	<input type="submit" value="Отправить">
 </form>
+<?php
+
+echo '<pre>';
+print_r($_FILES);
+echo '</pre>';
+
+
+if(isset($_FILES["uploadFile"]["name"]) && !empty($_FILES["uploadFile"]["name"])){
+	if($_FILES["uploadFile"]["error"] == UPLOAD_ERR_OK && move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $_FILES["uploadFile"]["name"])){
+		echo "Файл загружен";
+	} else{
+		echo "Ошибка";
+	}
+}
+?>
