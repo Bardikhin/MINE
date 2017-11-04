8071aa083046458820964e4a4303d21331b22d1f
Bardikhin <32037797+Bardikhin@users.noreply.github.com>
2017-11-04 18:10:26 +0200

    lesson2.2

--- admin.php
+++ admin.php
@@ -1,20 +1,27 @@
-<form method="post" action="" enctype="multipart/form-data">
-	<input type="file" name="uploadFile"><br/>
-	<input  type="hidden" name="MAX_FILE_SIZE" value="900000000"  /><br/>
-	<input type="submit" value="Отправить">
-</form>
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="utf-8"> 
+	<title>Загрузка тестов</title> 
+</head> 
+<body> 
+	<h2>Загрузка тестов</h2> 
+	<form enctype="multipart/form-data" method="post"> 
+		<p>Добрый день, загрузите, пожалуйста, пару тестов и перейдите на "Выбор тестов"</p> 
+		<p><input type="file" name="uploadFile"></p> 
+		<p><input type="hidden" name="MAX_FILE_SIZE" value="50000"></p> 
+		<p><input type="submit" value="Отправить"></p> 
+	</form>
+	<a href="list.php">Выбор тестов</a> 
+</body>
+</html>
 <?php
-
-echo '<pre>';
-print_r($_FILES);
-echo '</pre>';
-
-
+$uploaddir = 'Tests/';
 if(isset($_FILES["uploadFile"]["name"]) && !empty($_FILES["uploadFile"]["name"])){
-	if($_FILES["uploadFile"]["error"] == UPLOAD_ERR_OK && move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $_FILES["uploadFile"]["name"])){
-		echo "Файл загружен";
+	if($_FILES["uploadFile"]["error"] == UPLOAD_ERR_OK && move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $uploaddir . $_FILES['uploadFile']['name'])){
+		echo 'Уже загружен(о) ' . (count(scandir($uploaddir))-2) . ' файл(ов)';
 	} else{
-		echo "Ошибка";
+	echo "Ошибка";
 	}
 }
 ?>
--- test.php
+++ test.php
@@ -1,12 +1,44 @@
 <?php
-
-if(isset($_FILES['myfile']['name']) && !empty($_FILES['myfile']['name'])){
-	if($_FILES['myfile']["error"] == UPLOAD_ERR_OK &&
-		move_uploaded_file($_FILES['myfile']['tmp_name'], $_FILES['myfile']['name'])){
-		echo "Файл загружен";
-	} else{
-		echo "Ошибка";
-	}
-}
-
+$dir ='Tests/'; 
+$file = file_get_contents($dir. $_GET['Test']);
+$data = json_decode($file, true);
 ?>
+<html>
+<head> 
+    <meta charset="utf-8"> 
+    <title>Процесс</title> 
+</head>
+<body>
+<h2>Тест</h2> 
+<form action="" method="POST">
+            <fieldset name="question1">
+                <legend><?= $data[0]['question']; ?></legend>
+                <label><input type="radio" name="question1" value='<?php echo $data[0]['answer1']; ?>'><?php echo $data[0]['answer1']; ?></label>
+                <label><input type="radio" name="question1" value='<?php echo $data[0]['answer2']; ?>'><?php echo $data[0]['answer2']; ?></label>
+                <label><input type="radio" name="question1" value='<?php echo $data[0]['answer3']; ?>'><?php echo $data[0]['answer3']; ?></label>
+                <label><input type="radio" name="question1" value='<?php echo $data[0]['answer4']; ?>'><?php echo $data[0]['answer4']; ?></label>
+            </fieldset>
+            <fieldset name="question2">
+                <legend><?= $data[1]['question']; ?></legend>
+                <label><input type="radio" name="question2" value='<?php echo $data[1]['answer1']; ?>'><?php echo $data[1]['answer1']; ?></label>
+                <label><input type="radio" name="question2" value='<?php echo $data[1]['answer2']; ?>'><?php echo $data[1]['answer2']; ?></label>
+                <label><input type="radio" name="question2" value='<?php echo $data[1]['answer3']; ?>'><?php echo $data[1]['answer3']; ?></label>
+                <label><input type="radio" name="question2" value='<?php echo $data[1]['answer4']; ?>'><?php echo $data[1]['answer4']; ?></label>
+            </fieldset>
+            <p><input type="submit" name="submit" value='Отправить'></p>
+<a href="list.php">Выбор тестов</a> 
+</form>
+<?php
+if (isset($_POST['question1'])) {
+    if ($_POST['question1'] == $data[0]['correct_answer']){
+    echo 'На первый вы ответили правильно <br/>';
+    } else echo "Неправильно ответили на первый вопрос <br/>";
+}
+if (isset($_POST['question2'])) {
+    if ($_POST['question2'] == $data[1]['correct_answer']){
+    echo 'На второй вы ответили правильно';
+    } else echo "Неправильно ответили на второй вопрос";
+}
+?>
+</body>
+</html>
