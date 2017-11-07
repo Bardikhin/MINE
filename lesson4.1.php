7992560e46b5cf6dd13bc90e297e371effda968e
Bardikhin <32037797+Bardikhin@users.noreply.github.com>
2017-11-07 22:18:04 +0200

    lesson4.1

--- lesson4.1.php
+++ lesson4.1.php
@@ -1,16 +1,58 @@
 <?php
+$host = 'localhost';
+$db = 'test';
+$user = 'root';
+$pass = null;
 
-/*$sql = file_get_contents('dump.txt');
-
-echo '<pre>';
-print_r($sql);
-echo '</pre>';*/
-
-
-
-$pdo = new PDO("mysql:host=localhost;dbname = global","barikhin","neto1339");
-$sql = "Select * from 'books'";
+$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+$sql = "Select * from books";
 $statement = $pdo->prepare($sql);
 $statement->execute();
 
+$results = [];
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+	$results[]=$row;
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+    	table { 
+       	 border-spacing: 0;
+       	 border-collapse: collapse;
+    	}
+  	  	table td, table th {
+    	  border: 1px solid #ccc;
+  	      padding: 5px;
+  	  	} 
+ 	   	table th {
+      	  background: #eee;
+   	 	}
+    </style>
+	<title></title>
+</head>
+<body>
+<table>
+	<tr>
+		<th>Название</th>
+		<th>Автор</th>
+		<th>Год выпуска</th>
+		<th>Жанр</th>
+		<th>ISBN</th>
+	</tr>
+	<tr>
+		<?php foreach ($results as $row) { ?>
+		<tr>
+			<td><?php echo htmlspecialchars($row['name']) ?></td>
+			<td><?php echo htmlspecialchars($row['author']) ?></td>
+			<td><?php echo $row['year'] ?></td>
+			<td><?php echo htmlspecialchars($row['genre']) ?></td>
+			<td><?php echo $row['isbn'] ?></td>
+		</tr>
+		<?php } ?>
+	</tr>
+</table>
+</body>
+</html>
